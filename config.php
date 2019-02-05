483c999a5a9f7116444d516eba47e328aa9cb3de
seltor <seltor@mail.ru>
2019-02-06 00:20:01 +0300

    20190205config

--- config.php
+++ config.php
@@ -1,18 +1,17 @@
 <?php
 // HTTP
-define('HTTP_SERVER', 'https://store.jktu.ru/admin/');
-define('HTTP_CATALOG', 'https://store.jktu.ru/');
+define('HTTP_SERVER', 'https://store.jktu.ru/');
 
 // HTTPS
-define('HTTPS_SERVER', 'https://store.jktu.ru/admin/');
-define('HTTPS_CATALOG', 'https://store.jktu.ru/');
+define('HTTPS_SERVER', 'https://store.jktu.ru/');
+
 
 // DIR
-define('DIR_APPLICATION', '/var/www/selto149/data/www/store.jktu.ru/admin/');
+define('DIR_APPLICATION', '/var/www/selto149/data/www/store.jktu.ru/catalog/');
 define('DIR_SYSTEM', '/var/www/selto149/data/www/store.jktu.ru/system/');
 define('DIR_IMAGE', '/var/www/selto149/data/www/store.jktu.ru/image/');
-define('DIR_LANGUAGE', '/var/www/selto149/data/www/store.jktu.ru/admin/language/');
-define('DIR_TEMPLATE', '/var/www/selto149/data/www/store.jktu.ru/admin/view/template/');
+define('DIR_LANGUAGE', '/var/www/selto149/data/www/store.jktu.ru/catalog/language/');
+define('DIR_TEMPLATE', '/var/www/selto149/data/www/store.jktu.ru/catalog/view/theme/');
 define('DIR_CONFIG', '/var/www/selto149/data/www/store.jktu.ru/system/config/');
 define('DIR_CACHE', '/var/www/selto149/data/www/store.jktu.ru/system/storage/cache/');
 define('DIR_DOWNLOAD', '/var/www/selto149/data/www/store.jktu.ru/system/storage/download/');
